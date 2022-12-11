af70f029176289640fdbdc339a1da91a9d60b453
jx11r <jx11r@hotmail.com>
2022-12-10 18:54:54 -0600

    core/keymaps: keymap -> map

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,28 +1,28 @@
-local keymap = vim.keymap
+local map = vim.keymap.set
 
 -- Reload init.lua
-keymap.set('n', '<Space>r', ':source $MYVIMRC<cr>')
+map('n', '<Space>r', ':source $MYVIMRC<cr>')
 
 -- Write / Quit
-keymap.set('n', '<Space>w', ':w<cr>')
-keymap.set('n', '<Space>q', ':q<cr>')
+map('n', '<Space>w', ':w<cr>')
+map('n', '<Space>q', ':q<cr>')
 
 -- Select All
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+map('n', '<C-a>', 'gg<S-v>G')
 
 -- Packer Stuff
-keymap.set('n', '<Space>pc', ':PackerCompile<cr>')
-keymap.set('n', '<Space>pi', ':PackerInstall<cr>')
-keymap.set('n', '<Space>ps', ':PackerSync<cr>')
+map('n', '<Space>pc', ':PackerCompile<cr>')
+map('n', '<Space>pi', ':PackerInstall<cr>')
+map('n', '<Space>ps', ':PackerSync<cr>')
 
 -- Color Highlighter
-keymap.set('n', '<C-c>', ':ColorizerToggle<cr>')
+map('n', '<C-c>', ':ColorizerToggle<cr>')
 
 -- File Explorer
-keymap.set('n', '<Space>e', ':NvimTreeFocus<cr>')
+map('n', '<Space>e', ':NvimTreeFocus<cr>')
 
 -- Fuzzy Finder
-keymap.set('n', '<Space>f', ':Telescope find_files<cr>')
+map('n', '<Space>f', ':Telescope find_files<cr>')
 
 -- Go to any word in the current buffer
-keymap.set('n', '<Space>s', ':HopWord<cr>')
+map('n', '<Space>s', ':HopWord<cr>')
