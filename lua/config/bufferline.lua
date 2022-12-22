d00c65a6c4f3188584733968dee7ad1c4ad56a35
jx11r <jx11r@hotmail.com>
2022-12-22 17:49:06 -0600

    bufferline: update config

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,8 +1,21 @@
 require('bufferline').setup {
   options = {
-    indicator = { style = 'none' },
+    always_show_bufferline = false,
+    indicator = { style = 'icon' },
+    name_formatter = function(buf)
+      return ' ' .. buf.name .. ' '
+    end,
+    max_name_length = 20,
     separator_style = 'thin',
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
+  },
+
+  highlights = require('catppuccin.groups.integrations.bufferline').get {
+    custom = {
+      mocha = {
+        fill = { bg = '#181825' },
+      },
+    },
   },
 }
