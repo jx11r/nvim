e2e88337a38f3dc5bd0954b26ec96644d01a2746
jx11r <jx11r@hotmail.com>
2022-12-22 19:58:39 -0600

    keymaps: add options

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 -- Reload init.lua
 map('n', '<Space>r', ':source $MYVIMRC<cr>')
@@ -16,19 +17,19 @@ map('n', '<Space>pi', ':PackerInstall<cr>')
 map('n', '<Space>ps', ':PackerSync<cr>')
 
 -- Color Highlighter
-map('n', '<C-c>', ':ColorizerToggle<cr>')
+map('n', '<C-c>', ':ColorizerToggle<cr>', opts)
 
 -- Tab Line
-map('n', '<Tab>', ':BufferLineCycleNext<cr>')
-map('n', '<C-Tab>', ':BufferLineCyclePrev<cr>')
-map('n', '<C-w>', ':bdelete!<cr>')
+map('n', '<Tab>', ':BufferLineCycleNext<cr>', opts)
+map('n', '<C-Tab>', ':BufferLineCyclePrev<cr>', opts)
+map('n', '<C-w>', ':bdelete!<cr>', opts)
 
 -- File Explorer
-map('n', '<Space>e', ':NvimTreeToggle<cr>')
+map('n', '<Space>e', ':NvimTreeToggle<cr>', opts)
 
 -- Fuzzy Finder
-map('n', '<Space>f', ':Telescope find_files<cr>')
-map('n', '<Space>g', ':Telescope live_grep<cr>')
+map('n', '<Space>f', ':Telescope find_files<cr>', opts)
+map('n', '<Space>g', ':Telescope live_grep<cr>', opts)
 
 -- Go to any word in the current buffer
-map('n', '<Space>s', ':HopWord<cr>')
+map('n', '<Space>s', ':HopWord<cr>', opts)
