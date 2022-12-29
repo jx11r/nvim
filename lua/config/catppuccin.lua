12137170fa90964e8294907fe1a1571838f2c300
jx11r <jx11r@hotmail.com>
2022-12-28 21:49:43 -0600

    plugin(catppuccin): add lsp

--- lua/config/catppuccin.lua
+++ lua/config/catppuccin.lua
@@ -17,6 +17,21 @@ catppuccin.setup {
     telescope = true,
     treesitter = true,
     ts_rainbow = true,
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { 'italic' },
+        hints = { 'italic' },
+        warnings = { 'italic' },
+        information = { 'italic' },
+      },
+      underlines = {
+        errors = { 'underline' },
+        hints = { 'underline' },
+        warnings = { 'underline' },
+        information = { 'underline' },
+      },
+    },
   },
 
   highlight_overrides = {
